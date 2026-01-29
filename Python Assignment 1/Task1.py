5f539784cabcd961afe3dce236e9bce662672b7d
Masrat Yousuf <masratyousuf0178@gmail.com>
2026-01-29 11:02:05 +0530

    Add files via upload

---
+++ Python Assignment 1/Task1.py
@@ -0,0 +1,17 @@
+
+num1= int(input("Enter the First Number"))
+num2= int(input("Enter the Second Number"))
+
+add= num1 + num2
+sub=num1-num2
+multi=num1*num2
+div=num1/num2
+
+print("Addition of two numbers is: ", add)
+print("Subtraction of two numbers is: ", sub)
+print("Multiplication of two numbers is: ", multi)
+print("Division of two numbers is: ", div)
+
+
+
+
---
+++ Python Assignment 1/Task2.py
@@ -0,0 +1,11 @@
+'''
+This Program Takes  user's first name and last name as input.
+Concatenates the first name and last name into a full name.
+Prints a personalized greeting message using the full name.
+'''
+
+fname= input("Enter Your first name: ")
+lname=input("Enter Your Last name: ")
+
+fname=fname + " " +lname
+print("hello", fname , "Welcome to python program")
